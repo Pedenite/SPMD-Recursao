8df94dd51908021d416b761f6b70a889ca566bc5
Pedenite <pedenite@gmail.com>
2019-10-30 22:27:18 -0300

    novas sequências

--- sequenciaRecursiva.js
+++ sequenciaRecursiva.js
@@ -1,18 +1,63 @@
 var n = 2
 var resultado = document.getElementById("resultado")
-resultado.innerHTML += `<td>1</td><td>2</td>`
+
+var func = Number(document.getElementById("funcao").value)
+function iniciarFunc(){
+    func = Number(document.getElementById("funcao").value)
+    if(func==1){
+        resultado.innerHTML = ` <tr><td>n</td><td>a[n]</td></tr><td>1</td><td>2</td>`
+    }
+    if(func==2){
+        resultado.innerHTML = ` <tr><td>n</td><td>a[n]</td></tr><td>1</td><td>1</td>`
+    }
+    if(func==3){
+        resultado.innerHTML = ` <tr><td>n</td><td>a[n]</td></tr><td>1</td><td>5</td>`
+    }
+    n = 2
+    seqAtual()
+}
 function seqAtual(){
     let txtSeq = document.getElementById("sequenciaAN")
-    txtSeq.innerText = `a[${n}] = a[${n-1}] + 2•${n}`
-    document.getElementById("numA1").placeholder = `a[${n-1}]`
-    document.getElementById("numSN").placeholder = `2*${n}`
-    document.getElementById("numAN").placeholder = `a[${n-1}] + 2 * ${n}`
+    if(func == 1){
+        txtSeq.innerText = `a[${n}] = a[${n-1}] + 2•${n}`
+        document.getElementById("numA1").placeholder = `a[${n-1}]`
+        document.getElementById("numSN").placeholder = `2*${n}`
+        document.getElementById("numAN").placeholder = `a[${n-1}] + 2 * ${n}`
+    }
+    if(func == 2){
+        txtSeq.innerText = `a[${n}] = a[${n-1}] + (2•${n}-1)`
+        document.getElementById("numA1").placeholder = `a[${n-1}]`
+        document.getElementById("numSN").placeholder = `(2*${n})-1`
+        document.getElementById("numAN").placeholder = `a[${n-1}] + (2*${n}-1)`
+    }
+    if(func == 3){
+        txtSeq.innerText = `a[${n}] = a[${n-1}] + (${n}+4)`
+        document.getElementById("numA1").placeholder = `a[${n-1}]`
+        document.getElementById("numSN").placeholder = `${n}+4`
+        document.getElementById("numAN").placeholder = `a[${n-1}] + (${n}+4)`
+    }
 }
 function calc(n){
-    if(n <= 1){
-        return 2
-    }else{
-        return (calc(n-1)+(2*n))
+    if(func == 1){   
+        if(n <= 1){
+            return 2
+        }else{
+            return (calc(n-1)+(2*n))
+        }
+    }
+    if(func == 2){   
+        if(n <= 1){
+            return 1
+        }else{
+            return (calc(n-1)+(2*n-1))
+        }
+    }
+    if(func == 3){   
+        if(n <= 1){
+            return 5
+        }else{
+            return (calc(n-1)+(n+4))
+        }
     }
 }
 function confirmar(){
@@ -20,7 +65,7 @@ function confirmar(){
     let an1 = Number(document.getElementById("numA1").value)
     let sn = Number(document.getElementById("numSN").value)
     //console.log(an+" "+typeof(an))
-    if(an == calc(n) && an1 == calc(n-1) && sn == 2*n){
+    if(an == calc(n) && an1 == calc(n-1) && sn == an-an1){
         document.getElementById("numAN").value = ""
         document.getElementById("numA1").value = ""
         document.getElementById("numSN").value = ""
@@ -42,7 +87,7 @@ function avancar_10(){
         //console.log(an+" "+typeof(an))
         an = calc(n)
         an1 = calc(n-1)
-        sn == 2*n
+        sn == an-an1
         document.getElementById("numAN").value = ""
         document.getElementById("numA1").value = ""
         document.getElementById("numSN").value = ""
