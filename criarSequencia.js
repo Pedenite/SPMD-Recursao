06e87343ab8bc9bbce6c43ef51fe7e82d7f7761e
KeydsonEstrela <keydsonestrela@hotmail.com>
2019-10-31 22:00:03 -0300

    Adições das caixa dos valores iniciais

--- criarSequencia.js
+++ criarSequencia.js
@@ -1,10 +1,8 @@
-var qtd = 0
+let qtd = Number(document.getElementById("valInit").value)
 
-function qtdVal(){
-    qtd = Number(document.getElementById("valInit").value)
-}
 function valoresInit(){
-    for(;qtd>0;qtd--){
-        document.getElementById("valorDeA").placeholder = `a[${qtd}]`
+    for(let i = 0;i < qtd; i++){
+        document.getElementById("valoresIniciais").innerHTML += `<input type="number" placeholder = "a[${i+1}]" name="" id="valorDeA" style="max-width: calc(65vh/3);margin-top: 15px;"><br>`
     }
+    
 }
