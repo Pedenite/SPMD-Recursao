377fb0181e9b3ec8ee99f8553958232a767ebff4
PedroPR26 <pedropessoaramos@gmail.com>
2019-10-31 22:30:04 -0300

    Pronto ate agr

--- criarSequencia.js
+++ criarSequencia.js
@@ -4,9 +4,12 @@ function valoresInit(){
     document.getElementById("infoQtd").className = "sumir"
     document.getElementById("buttonAN").className = "sumir"
     document.getElementById("valInit").className = "sumir"
-    document.getElementById("valoresIniciais").innerHTML += `<p>Informe os valores dos elementos conforme indicado:</p><form action="javascript:">`
+    document.getElementById("valoresIniciais").innerHTML += `<p>Informe os valores dos elementos conforme indicado:</p><form action="javascript:addValoresInit()">`
     for(let i = 0;i < qtd; i++){
         document.getElementById("valoresIniciais").innerHTML += `<input type="number" placeholder = "a[${i+1}]" name="" id="valorDeA" style="max-width: calc(65vh/3);margin-top: 15px;"><br>`
     }
     document.getElementById("valoresIniciais").innerHTML += `<button class="button" type="submit">Confirmar</button></form>`
+}
+function addValoresInit(){
+    alert("8====D")
 }
