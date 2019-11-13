3d12bcf92b8d0f2d26d278a9d72bed86e9ee3c47
KeydsonEstrela <keydsonestrela@hotmail.com>
2019-11-13 13:57:11 -0200

    Implementação do FeedBack

--- sequenciaRecursiva.js
+++ sequenciaRecursiva.js
@@ -1,7 +1,20 @@
 var n = 2
 var resultado = document.getElementById("resultado")
-
 var func = Number(document.getElementById("funcao").value)
+
+//Vars FeedBack
+var acertos = 0
+var erros = 0
+var contAjuda = 0
+var cont = 0
+var controlError
+var controlHint
+
+function controlador(){ //Função para saber se ainda está na mesma questão ou não
+    controlError = 0
+    controlHint = 0
+}
+
 function iniciarFunc(){
     func = Number(document.getElementById("funcao").value)
     if(func==1){
@@ -16,7 +29,10 @@ function iniciarFunc(){
     n = 2
     seqAtual()
     document.getElementById("btn-avancar10").style.display = ''
+    zerarContadores()
+    controlador()
 }
+
 function seqAtual(){
     let txtSeq = document.getElementById("sequenciaAN")
     if(func == 1){
@@ -38,6 +54,7 @@ function seqAtual(){
         document.getElementById("numAN").placeholder = `a[${n-1}] + (n+4)`
     }
 }
+
 function calc(n){
     if(func == 1){   
         if(n <= 1){
@@ -61,10 +78,13 @@ function calc(n){
         }
     }
 }
+
 function confirmar(){
     let an = Number(document.getElementById("numAN").value)
     let an1 = Number(document.getElementById("numA1").value)
     let sn = Number(document.getElementById("numSN").value)
+
+
     if(an == calc(n) && an1 == calc(n-1) && sn == an-an1){
         document.getElementById("numAN").value = ""
         document.getElementById("numA1").value = ""
@@ -73,6 +93,15 @@ function confirmar(){
         n++
         document.getElementById("numA1").focus()
         seqAtual()
+        if(controlHint == 1){
+            contAjuda++
+            controlador()
+        }else if(controlError == 1){
+            cont++
+            controlador()
+        }else{
+            acertos++
+        }
     }else{
         if(func == 1){
             window.alert(`Errado!\n\nNote que:  a[${n}] = a[${n-1}] + 2•n`)
@@ -83,22 +112,14 @@ function confirmar(){
         if(func == 3){
             window.alert(`Errado!\n\nNote que:  a[${n}] = a[${n-1}] + (n+4)`)
         }
+        if(controlError == 0){
+            erros++
+            controlError = 1
+        }
     }
     
 }
 
-function ajuda(){
-    if(func == 1){
-        window.alert(`a[${n}] = a[${n-1}] + 2•${n}`)
-    }
-    if(func == 2){
-        window.alert(`a[${n}] = a[${n-1}] + ((2•${n})-1)`)
-    }
-    if(func == 3){
-        window.alert(`a[${n}] = a[${n-1}] + (${n}+4)`)
-    }
-
-}
 function avancar_10(element){
     if(func != 0){
         let n2 = n+10
@@ -117,5 +138,37 @@ function avancar_10(element){
         }
         document.getElementById("numA1").focus()
         seqAtual()
+        controlador()
     }
+}
+
+function ajuda(){
+    if(func == 1){
+        window.alert(`a[${n}] = a[${n-1}] + 2•${n}`)
+    }
+    if(func == 2){
+        window.alert(`a[${n}] = a[${n-1}] + ((2•${n})-1)`)
+    }
+    if(func == 3){
+        window.alert(`a[${n}] = a[${n-1}] + (${n}+4)`)
+    }
+    if(controlHint == 0){
+        controlHint = 1
+    }
+}
+
+function feedBack(){ //Retorna o feedBack em uma caixa de diálogo
+    if(acertos > ((cont/2)+5)){
+        window.alert(`Parabéns!!!\n\nVocê teve mais acertos que a média!\n\nAcertos: ${acertos}\nErros: ${erros}\nResolvidas com Ajuda: ${contAjuda}`)
+    }
+    if(acertos < ((cont/2)+5)){
+        window.alert(`Exercite um Pouco Mais!\n\nVocê teve acertos inferior à média\n\nAcertos: ${acertos}\nErros: ${erros}\nResolvidas com Ajuda: ${contAjuda}`)
+    }
+}
+
+function zerarContadores(){
+    acertos = 0
+    erros = 0
+    contAjuda = 0
+    cont = 0
 }
